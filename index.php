d13e6c77e11b7cc45045dcbc8fb35f94c13a9569
sksat <sksat.tec@gmail.com>
2017-08-15 15:56:17 +0900

    javascript test

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
 <body>
 hoge
+
+<script type="text/javascript">
+var today = new Date();
+var month = today.getMonth()+1;
+var day   = today.getDate();
+document.write(month + "/" + day);
+</script>
+
 </body>
 </html>
